53f410c72dd6afdfc87cab5a31e3b768788ec2c1
Sumit <sumitbasu116@gmail.com>
2025-11-14 21:27:17 +0530

    ThreeSumClosest solution

--- ThreeSumClosest.java
+++ ThreeSumClosest.java
@@ -4,22 +4,22 @@ class Solution {
     public int threeSumClosest(int[] a, int target) {
         Arrays.sort(a);
         int n = a.length;
-        int diff = 9999;
-        int resSum=-1;
+        int diff = Integer.MAX_VALUE;;
+        int resSum=0;
         for(int i=0;i<n-2;i++){
             int left=i+1,right=n-1;
             while(left<right){
                 int sum = a[i] + a[left] + a[right];
-                int d = Math.abs(sum-target);
+                int d = Math.abs(target-sum);
                 if(d < diff){
                     diff = d;
                     resSum=sum;
                 }
-                if(resSum==target){
-                    return resSum;
-                } else if(resSum>target){
+                if(sum==target){
+                    return sum;
+                } else if(sum>target){
                     right--;
-                }else if(resSum<target){
+                }else if(sum<target){
                     left++;
                 }
             }
