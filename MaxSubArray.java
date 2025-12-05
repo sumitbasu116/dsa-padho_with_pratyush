31cf2ad23252c8a7e36816b3cb599b06f34c01ef
Sumit <sumitbasu116@gmail.com>
2025-12-05 21:14:47 +0530

    MaxSubArray comment

--- MaxSubArray.java
+++ MaxSubArray.java
@@ -3,8 +3,8 @@ https://leetcode.com/problems/maximum-subarray/description/
 
 class Solution {
     public int maxSubArray(int[] a) {
-        int best = a[0];
-        int ans = a[0];
+        int best = a[0];// 0th index me ek hi subarray possible hai and wahi best hai
+        int ans = a[0];// 0th index me ek hi subarray possible hai and wahi ans hai
         int n = a.length;
         for(int i=1;i<n;i++){
             int v1 = best + a[i];
